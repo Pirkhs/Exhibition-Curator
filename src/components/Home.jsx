3cb56791040e2d9a86da359ef2878cfc1ad27898
Pirkhs <nathan.briers32@gmail.com>
2024-11-07 20:02:59 +0000

    fix for redirecting on live version

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,11 +6,13 @@ export default function Home () {
         <summary className="single-text-center">
             <p> Welcome to Exhibition Curator ! </p>
         </summary>
-        <h2> Don't know where to start ? </h2>
-        <p> Start by clicking on the 'View Collections' tab to browse the various collections</p>
-        <p> Filter/Search/Sort through the collections to find your perfect piece of art</p>
-        <p> 'View More' info about a specific artwork and choose whether to add it to your own personal exhibition </p>
-        <p> Keep track of all your artworks via the 'My Exhibition' tab </p>
+        <div className="introduction">
+            <h2> Don't know where to start ? </h2>
+            <p> Start by clicking on the 'View Collections' tab to browse the various collections</p>
+            <p> Filter/Search/Sort through the collections to find your perfect piece of art</p>
+            <p> 'View More' info about a specific artwork and choose whether to add it to your own personal exhibition </p>
+            <p> Keep track of all your artworks via the 'My Exhibition' tab </p>
+        </div>
     </div>
     )
 }
